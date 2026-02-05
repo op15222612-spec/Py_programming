855d764098bab2c0f6a4eec89a55ae08e03712c6
ompatil <op15222612@eng.rizvi.edu.in>
2026-02-05 16:18:19 +0530

    experiment5.py
    Added task management functionality including adding, updating, removing, and sorting tasks.

--- experiment5.py
+++ experiment5.py
@@ -1,6 +1,10 @@
-cet = {"om", "Bob"}
-jee = {"Tathagat", "Bob"}
-neet = {"Anushman", "Eve"}
-print("All students:", cet|jee|neet)
-print("Students in all exam:", cet & jee & neet)
-print("Cet but not in jee:", cet - jee)
+tasks = []
+tasks.append("Study")
+tasks.append("Exercise")
+tasks.append("Sleep")
+tasks[1] = "Play" #update task
+tasks.remove("Sleep") #remove task 
+tasks.sort() #sort tasks
+task_tuple = tuple(tasks)
+for t in task_tuple:
+    print(t)
