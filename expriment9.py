fa0e21e749e7f7ae3ef16b166c1db2d2886b380e
ompatil <op15222612@eng.rizvi.edu.in>
2026-02-10 14:25:06 +0530

    expriment9.py

--- expriment9.py
+++ expriment9.py
@@ -1,5 +1,5 @@
-n=int(input("Enter a number:"))
-fact=1
+n = int (input("Enter a number:"))
+fact = 1
 for i in range(1,n+1):
-    fact = fact*1
-    print("Factorial is :",fact)
+    fact = fact*i
+    print("Factorial is:",fact)
